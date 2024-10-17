fa64696a19ef378d58f0f7d64c3df18151392341
arnold714 <116993794+arnold714@users.noreply.github.com>
2024-10-17 14:37:00 +0900

    [Silver II] Title: N과 M (12), Time: 176 ms, Memory: 20300 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -0,0 +1,67 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main{
+	static int N, M;
+	static int[] arr;
+	static int[] ans;
+	static StringBuilder sb;
+	static String str;
+	static Set<String> set;
+	static boolean[] visited;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		sb = new StringBuilder();
+		set = new HashSet<>();
+		arr = new int[N];
+		ans = new int[M];
+		for (int n = 0; n < N; n++) {
+			arr[n] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		for (int i = 0; i < N; i++) {
+			ans[0] = arr[i];
+			recur(i, 1);
+		}
+		System.out.println(sb.toString());
+	}
+
+	private static void recur(int cur, int cnt) {
+		if (cnt == M) {
+			StringBuilder sb2 = new StringBuilder();
+			for (int i = 0; i < M; i++) {
+				sb2.append(ans[i] + " ");
+			}
+			sb2.append("\n");
+			if (set.add(sb2.toString())) {
+				sb.append(sb2.toString());
+			}
+			return;
+		}
+
+		if (cur >= N)
+			return;
+
+		for (int i = cur; i < N; i++) {
+			ans[cnt] = arr[i];
+			recur(i, cnt + 1);
+		}
+
+	}
+}
