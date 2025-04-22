e678d31d4a9fdf8842ae262674d0a065e3256257
arnold714 <116993794+arnold714@users.noreply.github.com>
2025-04-22 16:46:20 +0900

    [Gold V] Title: A와 B 2, Time: 100 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Gold/12919. A와 B 2/A와 B 2.java
+++ 백준/Gold/12919. A와 B 2/A와 B 2.java
@@ -1,33 +1,71 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    static String S, T;
-    static int result = 0;
+    static int targetSize, answer;
+    static String T, S;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         S = br.readLine();
+        targetSize = S.length();
         T = br.readLine();
 
-        dfs(T);
-        System.out.println(result);
+        Deque<Character> dq = new LinkedList<>();
+        for (char c : T.toCharArray()) {
+            dq.addLast(c);
+        }
+
+        dfs(dq, true);
+        System.out.println(answer);
     }
 
-    public static void dfs(String current) {
-        if (current.length() == S.length()) {
-            if (current.equals(S)) {
-                result = 1;
+    public static void dfs(Deque<Character> dq, boolean isStraight) {
+        if (dq.size() == targetSize) {
+            StringBuilder sb = new StringBuilder();
+            if (isStraight) {
+                for (char c : dq) sb.append(c);
+            } else {
+                Iterator<Character> it = dq.descendingIterator();
+                while (it.hasNext()) sb.append(it.next());
+            }
+
+            if (sb.toString().equals(S)) {
+                answer = 1;
             }
             return;
         }
 
-        if (current.endsWith("A")) {
-            dfs(current.substring(0, current.length() - 1));
-        }
+        if (isStraight) {
+            // 뒤에 A 제거 가능
+            if (dq.peekLast() != null && dq.peekLast() == 'A') {
+                dq.pollLast();
+                dfs(dq, isStraight);
+                dq.offerLast('A');
+            }
 
-        if (current.startsWith("B")) {
-            StringBuilder sb = new StringBuilder(current.substring(1));
-            dfs(sb.reverse().toString());
+            // 앞에 B 제거 + 방향 뒤집기
+            if (dq.peekFirst() != null && dq.peekFirst() == 'B') {
+                dq.pollFirst();
+                dfs(dq, !isStraight);
+                dq.offerFirst('B');
+            }
+
+        } else {
+            // 역방향일 경우 처리 반대
+            // 앞에 A 제거 가능
+            if (dq.peekFirst() != null && dq.peekFirst() == 'A') {
+                dq.pollFirst();
+                dfs(dq, isStraight);
+                dq.offerFirst('A');
+            }
+
+            // 뒤에 B 제거 + 방향 전환
+            if (dq.peekLast() != null && dq.peekLast() == 'B') {
+                dq.pollLast();
+                dfs(dq, !isStraight);
+                dq.offerLast('B');
+            }
         }
     }
 }
